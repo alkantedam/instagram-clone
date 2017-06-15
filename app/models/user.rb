85a9976f30fe3b03ec8c7ea1eabcbc2fc9056dfb
Alina Dyussupova <alkantedam@gmail.com>
2017-06-15 06:48:02 +0000

    Instagram clone first commit

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+    
+    has_secure_password
+    
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    
+    validates :login, presence: true, uniqueness: true, length: {minimum: 3, maximum: 12}
+    validates :email, presence: true, uniqueness: {case_sensitive: false}, format: VALID_EMAIL_REGEX
+    validates :password, length: {minimum: 6}
+    
+    before_save {self.email = email.downcase}
+    
+    has_secure_password
+end
